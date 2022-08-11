08dd645aab22c50d7f796ff4f31aab699c3b9ddb
Simon Vanderschaeghe <steentje34@users.noreply.github.com>
2022-08-11 11:45:47 +0200

    MLM compatibility (#1)
    * MLM compatibility
    * Create README.md

--- ImageMapClassRadio.php
+++ ImageMapClassRadio.php
@@ -6,37 +6,39 @@ namespace uzgent\ImageMapClassRadio;
 class ImageMapClassRadio extends \ExternalModules\AbstractExternalModule {
 
     const annotation = "@CUSTOM_IMAGEMAP_RADIO";
-    public function redcap_data_entry_form($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance)
-    {
+
+    public function redcap_data_entry_form($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance) {
         $keyLabelCodeMap = [];
 
         foreach ($this->getMetadata($project_id, $instrument) as $field) {
             $field_annotation = $field['field_annotation'];
             if (strpos($field_annotation, self::annotation) !== false) {
+                $jsCall = str_replace(self::annotation . '=', "", $field_annotation);
                 $fieldname = $field['field_name'];
                 $keyLabelCodeMap[$fieldname]['label'] = html_entity_decode($field['field_label']);
-                $keyLabelCodeMap[$fieldname]['script'] = "try{".str_replace(self::annotation . '=', "", $field_annotation).'}catch(e) {console.warn(e);}' ;
+                $keyLabelCodeMap[$fieldname]['script'] = "try{" . $jsCall . '}catch(e) {console.warn(e);}';
             }
         }
-
+        $render_mode = version_compare(REDCAP_VERSION, "12.2.10", "ge") ? "afterRender" : (version_compare(REDCAP_VERSION, "12.0.0", "ge") ? "onLangChanged" : "beforeMLM");
+        $jsmo_name = $this->getJavascriptModuleObjectName();
+        $this->initializeJavascriptModuleObject();
         echo '<script>';
-        print file_get_contents(__DIR__ . "/imagemapfunctions.js");
+        print str_replace(["JSMONAME", "RENDERMODE"], [$jsmo_name, $render_mode], file_get_contents(__DIR__ . "/imagemapfunctions.js"));
         print file_get_contents(__DIR__ . "/imagemap.js");
-        print "\n";
+        print "\n$(function() {\n";
         $this->activateMapScripts($keyLabelCodeMap);
+        print "});\n";
         echo '</script>';
     }
-    
-    public function redcap_survey_page($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance)
-    {
+
+    public function redcap_survey_page($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance) {
         $this->redcap_data_entry_form($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance);
     }
 
-    function activateMapScripts(array $keyLabelCodeMap)
-    {
-        foreach ($keyLabelCodeMap as $key => $map)
-        {
-            echo $map["script"]."\n";
+    function activateMapScripts(array $keyLabelCodeMap) {
+        foreach ($keyLabelCodeMap as $key => $map) {
+            echo $map["script"] . "\n";
         }
     }
+
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+# Custom Imagemap with radiobuttons
+The external module allows you to host your own image map. This module allows for one selection at a time and links the data to a radiobutton.
